342b2e1b1b9241dc59548aecda26be20cecdd8e8
Sen1van <dmitryleontiev16@gmail.com>
2026-04-02 06:43:13 +0300

    Обновил зависимости, добавил обработку неизвестных URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './components/pages/Index.tsx'
-import Navigation from './components/base/navigation.tsx';
-import { ThemeProvider } from "@/components/theme-provider"
-import { Footer } from './components/base/footer.tsx';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./components/pages/Index.tsx";
+import NotFoundPage from "./components/pages/404.tsx";
+import Navigation from "./components/base/navigation.tsx";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Footer } from "./components/base/footer.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </ThemeProvider>
       <Footer />
     </BrowserRouter>
