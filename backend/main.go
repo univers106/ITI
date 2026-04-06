7f340b3fca0c2caf29a7c031487142642b32b2bb
Sen1van <dmitryleontiev16@gmail.com>
2026-04-06 23:22:45 +0300

    минимальное helloworld на echo

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"net/http"
+
+	"github.com/labstack/echo/v5"
 )
 
 func main() {
-	// Register a handler function for the default path "/"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Docker!")
-	})
+	e := echo.New()
+	e.GET("/", hello)
 
-	// Start the HTTP server on port 8080
-	fmt.Println("Server starting on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Server failed: %s\n", err)
+	if err := e.Start(":8080"); err != nil {
+		slog.Error("failed to start server", "error", err)
 	}
+}
+
+func hello(c *echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
